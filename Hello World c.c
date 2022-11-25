857f28e34dd0beb004a004840bba20987605296e
ErrolCord <errolcorderoc@gmail.com>
2022-11-25 14:49:18 -0600

    Input de texto agregado

--- Hello World c.c
+++ Hello World c.c
@@ -1,6 +1,9 @@
 //Clase 12 24/11/2022 Errol Cordero
 #include <stdio.h>
+char text [30];
 int main() {
-   printf("Hello, World!");
-   return 0;
+	printf("Type a text, less than 30 characters without spaces: \n");
+	scanf("%s", text);
+	printf("%s", text);
+	return 0;
 }

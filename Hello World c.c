24342f5afed93580912288f5ed94a7b6348b4c69
ErrolCord <errolcorderoc@gmail.com>
2022-11-25 18:25:32 -0600

    Se agrega posibilidad de modificar el texto

--- Hello World c.c
+++ Hello World c.c
@@ -1,9 +1,23 @@
 //Clase 12 24/11/2022 Errol Cordero
 #include <stdio.h>
 char text [30];
+int selec = 0;
+int out = 2;
+int string (int selec);
+
 int main() {
+	while (selec != out);{
+	string(selec);
+	}
+
+	return 0;
+}
+
+int string (int selec){
 	printf("Type a text, less than 30 characters without spaces: \n");
 	scanf("%s", text);
-	printf("%s", text);
-	return 0;
+	printf("%s\n", text);
+	printf("Do you want to Type another text? \n");
+	scanf("%d", selec);	
+	return selec;
 }
